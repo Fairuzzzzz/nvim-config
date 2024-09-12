6504e0112f0f5621ac05b6e9641b136701b76b4a
fairuz <fairuz.afnan@gmail.com>
2024-09-12 14:49:21 +0700

    add linter and git integration

---
+++ lua/plugins/autoformatting.lua
@@ -0,0 +1,37 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				go = { "goimports", "golines", "gofumpt" },
+				lua = { "stylua" },
+				javascript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				typescript = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				markdown = { "prettier" },
+				yaml = { "prettier" },
+				python = { "ast_grep" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
+}
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,35 +1,52 @@
 return {
-    'williamboman/mason.nvim',
-    dependencies = {
-        'williamboman/mason-lspconfig.nvim',
-    },
-    config = function()
-        local mason = require('mason')
-        
-        local mason_lspconfig = require('mason-lspconfig')
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSetDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
 
-        mason.setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗"
-                }
-            }
-        })
-        mason_lspconfig.setup({
-            ensure_installed = {
-                'gopls',
-                'lua_ls',
-                'clangd',
-                'ast_grep',
-                'html',
-                'cssls',
-                'dockerls',
-                'marksman',
-                'pyright'
-            },
-            automatic_installation = true, -- not the same as ensure_installed
-        })
-    end,
+		local mason_lspconfig = require("mason-lspconfig")
+
+		local mason_tool_installer = require("mason-tool-installer")
+
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"gopls",
+				"lua_ls",
+				"clangd",
+				"ast_grep",
+				"html",
+				"cssls",
+				"dockerls",
+				"marksman",
+				"pyright",
+			},
+			automatic_installation = true, -- not the same as ensure_installed
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"stylua",
+				"ast_grep",
+				"goimports",
+				"golines",
+				"gofumpt",
+				"ast_grep",
+				"eslint_d",
+				"golangci-lint",
+			},
+		})
+	end,
 }
